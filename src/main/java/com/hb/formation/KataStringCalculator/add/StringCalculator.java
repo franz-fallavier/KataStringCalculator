717f28bc99715fa96505750feb7fdd4ac969065a
Franz Fallavier <ffallavier@gmail.com>
2016-03-10 12:12:07 +0100

    Comment & Alina's Repository

--- src/main/java/com/hb/formation/KataStringCalculator/add/StringCalculator.java
+++ src/main/java/com/hb/formation/KataStringCalculator/add/StringCalculator.java
@@ -4,17 +4,19 @@ package com.hb.formation.KataStringCalculator.add;
 public class StringCalculator {
 
 	/**
-	 * Addition numbers
-	 * @param numbers 
+	 * Addition numbers from String
+	 * 
+	 * @param numbers
 	 * @return the sum of numbers
 	 */
 	public int add(String numbers) {
 		int sum = 0;
-		if ( !numbers.isEmpty() ) {
-			String [] splitedNumbers = numbers.split(",");
-			for(String number: splitedNumbers) {
-				sum += Integer.parseInt(number);			
-			}			
+		if (!numbers.isEmpty()) {
+			// Get Numbers in table
+			String[] splitedNumbers = numbers.split(",");
+			for (String number : splitedNumbers) {
+				sum += Integer.parseInt(number);
+			}
 		}
 		return sum;
 	}
--- src/test/java/com/hb/formation/KataStringCalculator/add/StringCalculatorTest.java
+++ src/test/java/com/hb/formation/KataStringCalculator/add/StringCalculatorTest.java
@@ -10,10 +10,10 @@ public class StringCalculatorTest {
 		// Given
 		StringCalculator sc = new StringCalculator();
 		String emptyString = "";
-		
+
 		// When
 		int result = sc.add(emptyString);
-		
+
 		// Then
 		Assert.assertEquals(result, 0);
 	}
@@ -21,25 +21,25 @@ public class StringCalculatorTest {
 	@Test
 	public void givenOneNumber_whenAddNumbers_thenSameNumber() {
 		// Given
-		StringCalculator sc = new StringCalculator();		
+		StringCalculator sc = new StringCalculator();
 		String oneNumber = "3";
-		
+
 		// When
 		int result = sc.add(oneNumber);
-		
+
 		// Then
 		Assert.assertEquals(result, Integer.parseInt(oneNumber));
 	}
-	
+
 	@Test
 	public void givenTwoNumbers_whenAddNumbers_thenSum() {
 		// Given
-		StringCalculator sc = new StringCalculator();		
+		StringCalculator sc = new StringCalculator();
 		String numbers = "3,5";
-		
+
 		// When
 		int result = sc.add(numbers);
-		
+
 		// Then
 		Assert.assertEquals(result, 8);
 	}
