9a4ead8300788875f515711dcee5e827fe44ebd3
Avl <alina.vlas@gmail.com>
2016-03-10 11:31:33 +0100

    Sum of two numbers + TU

--- src/main/java/com/hb/formation/KataStringCalculator/add/StringCalculator.java
+++ src/main/java/com/hb/formation/KataStringCalculator/add/StringCalculator.java
@@ -3,14 +3,19 @@ package com.hb.formation.KataStringCalculator.add;
 
 public class StringCalculator {
 
+	/**
+	 * Addition numbers
+	 * @param numbers 
+	 * @return the sum of numbers
+	 */
 	public int add(String numbers) {
-		int count = numbers.split(",").length;
-		System.out.println(count);
-		// TODO verify content
-		if (count == 1) {
-			int first = Integer.parseInt(numbers);
-			return first;
+		int sum = 0;
+		if ( !numbers.isEmpty() ) {
+			String [] splitedNumbers = numbers.split(",");
+			for(String number: splitedNumbers) {
+				sum += Integer.parseInt(number);			
+			}			
 		}
-		return 0;
+		return sum;
 	}
 }
--- src/test/java/com/hb/formation/KataStringCalculator/add/StringCalculatorTest.java
+++ src/test/java/com/hb/formation/KataStringCalculator/add/StringCalculatorTest.java
@@ -6,24 +6,41 @@ import org.junit.*;
 public class StringCalculatorTest {
 
 	@Test
-	public void givenEmptyString_WhenAddNumbers_ThenResultZero() {
+	public void givenEmptyString_whenAddNumbers_thenResultZero() {
 		// Given
 		StringCalculator sc = new StringCalculator();
 		String emptyString = "";
+		
 		// When
 		int result = sc.add(emptyString);
+		
 		// Then
 		Assert.assertEquals(result, 0);
 	}
 
 	@Test
-	public void givenOneNumber_WhenAddNumbers_ThenSameNumber() {
+	public void givenOneNumber_whenAddNumbers_thenSameNumber() {
 		// Given
-		StringCalculator sc = new StringCalculator();
+		StringCalculator sc = new StringCalculator();		
 		String oneNumber = "3";
+		
 		// When
 		int result = sc.add(oneNumber);
+		
 		// Then
 		Assert.assertEquals(result, Integer.parseInt(oneNumber));
+	}
+	
+	@Test
+	public void givenTwoNumbers_whenAddNumbers_thenSum() {
+		// Given
+		StringCalculator sc = new StringCalculator();		
+		String numbers = "3,5";
+		
+		// When
+		int result = sc.add(numbers);
+		
+		// Then
+		Assert.assertEquals(result, 8);
 	}
 }
